6afec071bc7e58090ecbcb5454febfc139879411
Darko Petrovic <darko.petrovic.85@gmail.com>
2017-08-20 23:29:29 +0200

    small fixes

--- server.js
+++ server.js
@@ -2,26 +2,13 @@ var express = require('express');
 var path = require('path');
 
 var app = express();
-var pathToApp = __dirname + '/public';
 
 // app.use('/static', express.static(__dirname + '/public'));
-app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(express.static(path.join(__dirname, 'build')));
 
 app.get("*",function(req, res){
-    console.log("send file", path.join(__dirname, 'public', 'index.html'));
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
-
-
-// app.get('*', (req, res) => {
-//     console.log("send file");
-//     res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
-// });
-
-// app.get('/', function(req, res) {
-//     res.sendFile(pathToApp + '/index.html');
-// });
 
 app.listen(3000, function () {
     console.log('Listening on port 3000!');
